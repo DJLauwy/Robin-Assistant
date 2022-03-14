ba914ed255c82ba89071d8de360fd184b7053c73
Hugo van der Geest <55272066+HugovanderGeest@users.noreply.github.com>
2022-03-14 23:00:37 +0100

    Data Overview list

---
+++ resources/views/list.blade.php
@@ -0,0 +1,45 @@
+
+@extends('layouts.overview')
+
+@section('title')
+    Wachtwoord vergeten
+@endsection
+
+@section('data')
+        <h2 class="list_h2">
+            <?php echo date("d F Y"); ?>
+        </h2>
+
+        <div class="list_arow-left">
+            <
+        </div>
+
+        <div class="list_arow-right">
+            >
+        </div>
+
+        <table class="datum">
+            <tr>
+                <th>Ma <br> 14</th>
+                <th class="list_day">Di <br> 15</th>
+                <th>Wo <br> 16</th>
+                <th>Do <br> 17</th>
+                <th>Vr <br> 18</th>
+                <th>Za <br> 19</th>
+                <th>Zo <br> 20</th>
+            </tr>
+
+        </table>
+
+
+@endsection
+
+@section('content')
+
+ <section class="list">
+
+
+
+ </section>
+
+@endsection
