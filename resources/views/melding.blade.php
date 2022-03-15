d7a4978f05270b5502d97acc6fd69d545f6300f6
Hugo van der Geest <55272066+HugovanderGeest@users.noreply.github.com>
2022-03-15 01:06:27 +0100

    Fix

--- resources/views/melding.blade.php
+++ resources/views/melding.blade.php
@@ -7,15 +7,37 @@
 @section('content')
     <section class="melding">
 
-        <div class="melding_notification">
-            <div class="melding_holder">
-                <h2>Melding</h2>
-                <p>Activiteit opstaan toegevoegd aan: Woensdag 24 Maart</p>
-            </div>
-            <div class="melding_select">
 
+        <div id="verwijder1">
+            <button class="verwijder" onclick="verwijderfuncie1()">Verwijder</button>
+
+
+            <div class="melding_notification">
+                <div class="melding_holder">
+                    <h2>Melding</h2>
+                    <p>Activiteit opstaan compleet uitgevoerd</p>
+                </div>
+                <div class="melding_select">
+
+                </div>
             </div>
         </div>
+
+        <script>
+            function verwijderfuncie1() {
+                var x = document.getElementById("verwijder1");
+                if (x.style.display === "none") {
+                    x.style.display = "block";
+                } else {
+                    x.style.display = "none";
+                }
+            }
+        </script> <br>
+
+
+        <div id="verwijder2">
+            <button class="verwijder" onclick="verwijderfuncie2()">Verwijder</button>
+
 
         <div class="melding_notification">
             <div class="melding_holder">
@@ -26,6 +48,23 @@
 
             </div>
         </div>
+        </div>
+
+        <script>
+            function verwijderfuncie2() {
+                var x = document.getElementById("verwijder2");
+                if (x.style.display === "none") {
+                    x.style.display = "block";
+                } else {
+                    x.style.display = "none";
+                }
+            }
+        </script>
+
 
     </section>
+
 @endsection
+
+
+
